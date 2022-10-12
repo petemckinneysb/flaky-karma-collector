c471ebb3f6c3989679a9e21bdc8d2c0630ecdcbe
Pete McKinney <91989516+petemckinneysb@users.noreply.github.com>
2022-10-12 11:31:49 -0500

    add sleep so my processor doesn't explode

--- index.ts
+++ index.ts
@@ -2,8 +2,9 @@ import { KarmaProcessExecutor } from './classes/karma-process-executor';
 import { TextFileExporter } from './classes/text-file-exporter';
 
 const numberOfRuns: number = process.env.QPP_FLAKY_RUNS ? +process.env.QPP_FLAKY_RUNS : 2;
-const MAX_RUNS: number = 10;
+const MAX_RUNS: number = 50;
 
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
 (async () => {
     if (numberOfRuns > MAX_RUNS) {
@@ -11,7 +12,7 @@ const MAX_RUNS: number = 10;
         return;
     }
 
-    console.log(`Running Tests [${numberOfRuns}] times at ~2 minutes for each run.`);
+    console.log(`Running Tests [${numberOfRuns}] times at ~1 minutes for each run.`);
     for (let i = 0; i < numberOfRuns; i++) {
         console.log(`[${i + 1}] Starting Test Run...`);
 
@@ -29,5 +30,7 @@ const MAX_RUNS: number = 10;
         }
 
         console.log(`[${i + 1}] Completed Run for seed: ${result.seed}. Failures Detected: ${result.isFailureResult}. Test Run Time: ${elapsedTime / 1000}s`);
+        console.log(`Suspending for 1 minute.`);
+        await sleep(60000);
     }
 })();
